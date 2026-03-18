d8fbe74ad15b0ec6d92a2fc9d3fabfcaf9c3ae5f
toluadejimi <toluadejimi@gmail.com>
2026-03-18 03:10:33 +0100

    ffgghhhhj

--- core/storage/framework/views/22db8d7f6df819ad34955275578a14a797a680c7.php
+++
@@ -1,3 +0,0 @@
-<?php echo  loadExtension('tawk-chat') ?>
-<?php echo  loadExtension('google-analytics') ?>
-<?php /**PATH /Applications/programs/loggsplug/core/resources/views/partials/plugins.blade.php ENDPATH**/ ?>
